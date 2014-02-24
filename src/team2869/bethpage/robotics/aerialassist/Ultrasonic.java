ff635cf3a79639647987e994f3497f96eed387eb
Leon <Leon@ultrabook>
2014-02-24 06:56:30 -0500

    Modified Ultrasonic extension class by adding distance unit options.

--- src/team2869/bethpage/robotics/aerialassist/Ultrasonic.java
+++ src/team2869/bethpage/robotics/aerialassist/Ultrasonic.java
@@ -5,13 +5,14 @@ import edu.wpi.first.wpilibj.AnalogChannel;
 /**
  * This Ultrasonic class stores the linear regression to transform voltages into
  * distances.
- *
+ * NOTE: Consider renaming this class to avoid confusion with the WPILib class with the same name.
  * @author BETHPAGE HIGH SCHOOL, 2014 TEAM #2869 - HARSHIL GARG
  */
 public class Ultrasonic extends AnalogChannel {
 
     private double distanceSlope = RobotMap.VOLT_DISTANCE_SLOPE;
     private double distanceIntercept = RobotMap.VOLT_DISTANCE_INTERCEPT;
+    private edu.wpi.first.wpilibj.Ultrasonic.Unit units = edu.wpi.first.wpilibj.Ultrasonic.Unit.kInches;
 
     /**
      * Constructs an Ultrasonic rangefinder that is wired at the specific
@@ -35,22 +36,28 @@ public class Ultrasonic extends AnalogChannel {
     public Ultrasonic(int moduleNumber, int channel) {
         super(moduleNumber, channel);
     }
-    
-    /**
-     * Finds the voltage of the Ultrasonic rangefinder. Voltage scales with
-     * increasing distance.
-     * 
-     * @return A voltage from 0V to approximately 2.5V.
-     */
-    public double getVoltage() {
-        return super.getVoltage();
-    }
+
+   public edu.wpi.first.wpilibj.Ultrasonic.Unit getUnits() {
+      return units;
+   }
+
+   public void setUnits(edu.wpi.first.wpilibj.Ultrasonic.Unit units) {
+      if(this.units == units)
+         return;
+      //we are changing units, so adjust the slope to convert from cm to inches or incshes to cm.
+      if(this.units == edu.wpi.first.wpilibj.Ultrasonic.Unit.kInches) 
+         distanceSlope *= 2.54; //now we are in cm.
+      else
+         distanceSlope /= 2.54; //now we are in inches.
+      
+      this.units = units;
+   }
     
     /**
      * Calculates the distance in inches or centimeters based on a linear
      * regression developed after testing the ultrasonic rangefinder.
      * 
-     * @return Distance in inches.
+     * @return Distance in the whichever units the distanceSlope and distanceIntercept have been calibrated for.
      */
     public double getDistance() {
         return getVoltage() * distanceSlope + distanceIntercept;
